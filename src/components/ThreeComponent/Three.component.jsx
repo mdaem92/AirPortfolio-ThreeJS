da9a4416dd5117b263f813695ba8fb2270ddc42c
tommy tommy <tommy.achaemenid@outlook.com>
2022-09-22 11:58:58 +0200

    refactor three component

--- src/components/ThreeComponent/Three.component.jsx
+++ src/components/ThreeComponent/Three.component.jsx
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react'
 import { OrbitControls, PerspectiveCamera ,Stars } from '@react-three/drei'
-
 import { AirportScene } from '../airport-scene/AirportScene.component'
 
 
@@ -22,9 +21,6 @@ const Three = () => {
                 rotateSpeed={0.6}
                 enableZoom
             />
-            {/* <ArrowHelper x={5} y={0} z={0} color={0xffff00} dir={new Vector3(0, 2, 0)} />
-            <ArrowHelper x={5} y={0} z={0} color={0x0000FF} />
-            <ArrowHelper x={5} y={0} z={0} color={0xff0000} dir={new Vector3(2, 0, 0)} /> */}
             <ambientLight args={['#ffffff', 0.2]} />
             <Stars radius={100} depth={200} count={5000} factor={4} saturation={0} fade speed={2} />
             <AirportScene camRef={camRef} orbitControlRef={orbitControlRef}/>
